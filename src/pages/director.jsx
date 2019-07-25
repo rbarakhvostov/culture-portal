1754be168f3430ccbec8e2aa3bd315cd5367975a
wofec <wofecchuan@gmail.com>
2019-07-25 23:42:14 +0400

    feat: add director page

--- src/pages/director.jsx
+++ src/pages/director.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import DirectorPage from '../components/DirectorPage/Director';
 
 const Director = () => (
   <>
     <Header />
     <Sidebar />
     <h1>director</h1>
+    <DirectorPage />
   </>
 );
 
