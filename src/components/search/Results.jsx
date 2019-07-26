80cddd21773415ec41ffba71d1b2c3614307560d
Vlad Mamontov <vld.mmntv@gmail.com>
2019-07-26 20:56:11 +0300

    feat: add search result component

---
+++ src/components/search/Results.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import uniqid from 'uniqid';
+import { useTranslation } from 'react-i18next';
+
+import useDirectorsNamespaces from '../../utils/useDirectorsNamespaces';
+
+const Searchbox = ({ filter }) => {
+  const namespaces = useDirectorsNamespaces();
+  const { t } = useTranslation(namespaces);
+  const results = namespaces.map((item) => t(`${item}:name`));
+
+  return (
+    <ul>
+      {results
+        .filter((item) => item.includes(filter))
+        .map((item) => (
+          <li key={uniqid()}>{item}</li>
+        ))}
+    </ul>
+  );
+};
+
+Searchbox.propTypes = {
+  filter: PropTypes.string.isRequired,
+};
+
+export default Searchbox;
