9effb98313bd4ab26d82c9212673d3820aef45a0
wofec <wofecchuan@gmail.com>
2019-07-28 16:01:07 +0400

    fix: add location prop

--- src/pages/director.jsx
+++ src/pages/director.jsx
@@ -1,21 +1,31 @@
 import React, { Suspense } from 'react';
+import PropTypes from 'prop-types';
 
 import Header from '../components/layout/Header';
 import Sidebar from '../components/layout/Sidebar';
 import Map from '../components/director/Map';
 
-const Director = (director) => (
-  <>
-    <Header />
-    <Sidebar />
-    {Map(director)}
-  </>
-);
+const Director = ({ location }) => {
+  console.log(location);
+  const director = location.state ? location.state.director : null;
+
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      {Map(director)}
+    </>
+  );
+};
 
 const DirectorWrapper = () => (
   <Suspense fallback="loading">
     <Director />
   </Suspense>
 );
+
+Director.propTypes = {
+  location: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default DirectorWrapper;
