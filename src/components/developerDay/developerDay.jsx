ab86adc4c89f426c67327b3268f83b0551ce4bfb
rbarakhvostov <rbarakhvostov@gmail.com>
2019-07-28 14:25:30 +0300

    feat: add basic structure of the block

--- src/components/developerDay/developerDay.jsx
+++ src/components/developerDay/developerDay.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
+import styles from './developerDay.css';
 
 const DeveloperDay = ({ director }) => {
   const { t } = useTranslation(director);
-  const info = [];
-  t('bio').forEach((item) =>
-    info.push(
-      <>
-        <div>{item.date}</div>
-        <div>{item.description}</div>
-      </>
-    )
+  // const info = ['Режиссёр Дня'];
+  // info.push(t('name'), t('date'), t('description'));
+  return (
+    <div className={styles.authorDayBlock}>
+      <h2>Author of the Day</h2>
+      <h3>{t('name')}</h3>
+      <h4>{t('date')}</h4>
+      <p>{t('description')}</p>
+      <img src={t('img')} alt="author of the day" />
+      <a href="google.com">go</a>
+    </div>
   );
-
-  return <div>{info}</div>;
 };
 
 DeveloperDay.propTypes = {
