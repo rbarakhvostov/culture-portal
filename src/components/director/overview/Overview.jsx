1bfc2ae615b7b3b7a9543997406a10f6766b9e89
wofec <wofecchuan@gmail.com>
2019-07-29 14:41:04 +0300

    feat: add basic overview

---
+++ src/components/director/overview/Overview.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import OverviewStyles from './overview.module.css';
+
+const Overview = ({ director }) => {
+  const { t } = useTranslation(director);
+  console.log(director);
+
+  return (
+    <div className={OverviewStyles.overviewWrapper}>
+      <img className={OverviewStyles.img} src={t('img')} alt="director image" />
+      <div className={OverviewStyles.info}>
+        <h1 className={OverviewStyles.name}>{t('name')}</h1>
+        <span className={OverviewStyles.date}>{t('date')}</span>
+        <span className={OverviewStyles.description}>{t('description')}</span>
+      </div>
+    </div>
+  )
+}
+
+Overview.propTypes = {
+  director: PropTypes.string.isRequired,
+};
+
+
+export default Overview;
