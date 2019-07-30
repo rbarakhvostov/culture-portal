5019b054ce72427d657f13ff36fb708c7805b9b9
rbarakhvostov <rbarakhvostov@gmail.com>
2019-07-30 10:42:36 +0300

    feat: add styles

--- src/components/directorOfTheDay/DirectorOfTheDay.jsx
+++ src/components/directorOfTheDay/DirectorOfTheDay.jsx
@@ -3,11 +3,16 @@ import { Link } from 'gatsby';
 import useDirectorsNamespaces from '../../utils/useDirectorsNamespaces';
 import Overview from '../director/overview/Overview';
 import chooseDirector from './chooseDirector';
+import directorOftheDayStyles from './directorOfTheDay.module.css';
 
 const DirectorOfTheDay = () => {
   const director = chooseDirector(useDirectorsNamespaces());
   return (
-    <Link to="/director/" state={{ director }}>
+    <Link
+      to="/director/"
+      state={{ director }}
+      className={directorOftheDayStyles.directorBlock}
+    >
       <Overview director={director} />
     </Link>
   );
