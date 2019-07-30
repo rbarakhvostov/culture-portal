79a379bfa64db25bc5fb800fa859b889349e4efa
rbarakhvostov <rbarakhvostov@gmail.com>
2019-07-30 10:19:20 +0300

    feat: change logic of the function

--- src/components/directorOfTheDay/chooseDirector.jsx
+++ src/components/directorOfTheDay/chooseDirector.jsx
@@ -1,30 +1,10 @@
 const chooseDirector = (directorNamespaces) => {
-  let director;
   const day = new Date();
-
-  switch (day.getDay()) {
-    case 1:
-    case 4:
-      [director] = [directorNamespaces[0]];
-      break;
-    case 2:
-    case 6:
-      [director] = [directorNamespaces[1]];
-      break;
-    case 3:
-      [director] = [directorNamespaces[0]];
-      break;
-    case 5:
-      [director] = [directorNamespaces[0]];
-      break;
-    case 0:
-      [director] = [directorNamespaces[0]];
-      break;
-    default:
-      [director] = [directorNamespaces[0]];
-  }
-
-  return director;
+  const director = directorNamespaces[day.getDate()];
+  if (director) return director;
+  return directorNamespaces[
+    Math.floor(Math.random() * directorNamespaces.length)
+  ];
 };
 
 export default chooseDirector;
