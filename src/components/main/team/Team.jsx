ca6b7e2b97ce9454279f464033fafbcb4a23ba4e
Vlad Mamontov <vld.mmntv@gmail.com>
2019-08-01 13:58:02 +0300

    feat: add team component styles

--- src/components/main/team/Team.jsx
+++ src/components/main/team/Team.jsx
@@ -2,20 +2,43 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import uniqid from 'uniqid';
 
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+
 import Developer from './Developer';
 
 const gitHubIds = ['rbarakhvostov', 'wofec', 'SvetlankaKuzyaka', 'vld-m'];
 
+const useStyles = makeStyles({
+  team: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  title: {
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: '4vh',
+  },
+  members: {
+    display: 'flex',
+    margin: '2vh 0',
+    justifyContent: 'center',
+  },
+});
+
 const Team = () => {
   const { t } = useTranslation('team');
+  const styles = useStyles();
 
   return (
-    <div>
-      <h1>{t('title')}</h1>
-      {gitHubIds.map((id) => (
-        <Developer id={id} key={uniqid()} />
-      ))}
-    </div>
+    <Container className={styles.team}>
+      <p className={styles.title}>{t('title')}</p>
+      <Container className={styles.members}>
+        {gitHubIds.map((id) => (
+          <Developer id={id} key={uniqid()} />
+        ))}
+      </Container>
+    </Container>
   );
 };
 
