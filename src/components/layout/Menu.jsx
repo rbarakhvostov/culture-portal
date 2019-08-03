984214390beeac1e5d9233a8820361d423afddc4
wofec <wofecchuan@gmail.com>
2019-08-03 14:34:08 +0300

    fix: clear code

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
-
 import SideBar from './Sidebar';
 
 const useStyles = makeStyles({
