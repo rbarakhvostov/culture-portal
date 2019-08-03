a435347c0d17bea1ae570264bbe347bbfdd5e5bb
Vlad Mamontov <vld.mmntv@gmail.com>
2019-08-03 10:52:27 +0300

    feat: add Material UI language switcher

--- src/components/layout/LanguageSwitcher.jsx
+++ src/components/layout/LanguageSwitcher.jsx
@@ -2,27 +2,75 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import uniqid from 'uniqid';
 
-import i18next from '../../utils/i18n';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+
+import getLanguage from '../../utils/i18n';
+
+const useStyles = makeStyles({
+  button: {
+    fontSize: '14px',
+    color: '#ffffff',
+    backgroundColor: '#607d8b',
+    '&:hover': {
+      backgroundColor: '#607d8b',
+    },
+  },
+  listItem: {
+    width: 64,
+    justifyContent: 'center',
+  },
+});
 
 const LanguageSwitcher = () => {
-  const languages = ['en', 'ru', 'by'];
+  const options = ['en', 'ru', 'by'];
+  const [anchorEl, setAnchorEl] = useState(null);
   const { i18n } = useTranslation('layout');
-  const [value, setValue] = useState(i18next.language.slice(0, 2));
+  const language = getLanguage();
+  const styles = useStyles();
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    i18n.changeLanguage(event.target.value);
+  const handleClose = (event, reason, lang) => {
+    if (lang) {
+      i18n.changeLanguage(lang);
+    }
+
+    setAnchorEl(null);
   };
 
   return (
-    <select value={value} onChange={handleChange}>
-      {languages.map((lang) => (
-        <option key={uniqid()} value={lang}>
-          {lang.toUpperCase()}
-        </option>
-      ))}
-    </select>
+    <>
+      <Button
+        className={styles.button}
+        onClick={handleClick}
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+      >
+        {language}
+      </Button>
+      <Menu
+        id="simple-menu"
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorEl={anchorEl}
+        keepMounted
+      >
+        {options.map((lang) => (
+          <MenuItem
+            className={styles.listItem}
+            onClick={() => handleClose(null, null, lang)}
+            key={uniqid()}
+          >
+            {lang.toUpperCase()}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
   );
 };
 
