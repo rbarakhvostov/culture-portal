bbc70fb7a42c92bc255685e322d5a3dfbdf32ccb
Vlad Mamontov <vld.mmntv@gmail.com>
2019-08-04 14:21:08 +0300

    fix: change search page styles

--- src/components/search/Results.jsx
+++ src/components/search/Results.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
+
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
 import DirectorItem from './DirectorItem';
-import getLangueage from '../../utils/i18n';
 import useDirectorsNamespaces from '../../utils/useDirectorsNamespaces';
-import ResultsStyles from './results.module.css';
+import getLangueage from '../../utils/i18n';
+
+const useStyles = makeStyles({
+  results: {
+    display: 'flex',
+    width: '75%',
+    margin: '30px auto',
+    flexDirection: 'column',
+  },
+});
 
 const Result = ({ filter }) => {
   const directorsData = useDirectorsNamespaces();
   const namespaces = Object.keys(directorsData);
   const { t } = useTranslation('layout');
+  const styles = useStyles();
   const results = {};
 
   const lng = getLangueage();
@@ -27,7 +39,7 @@ const Result = ({ filter }) => {
   );
 
   return (
-    <Paper className={ResultsStyles.results}>
+    <Paper className={styles.results}>
       <ul>
         {filtered.map((director) => (
           <DirectorItem director={director} key={director} />
